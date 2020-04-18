75bf5f9b1125c01bbc73464e00814e03ba02206a
Yevhen Shymotyuk <fly1ngdream@pm.me>
2020-04-18 18:36:14 +0300

    Update webpack config

--- {{cookiecutter.project_name}}/django/webpack/webpack.config.js
+++ {{cookiecutter.project_name}}/django/webpack/webpack.config.js
@@ -1,43 +1,61 @@
-const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({})
-    ],
-  },
-  entry: './index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'bundles')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: require.resolve('jquery'),
-        use: [{
-          loader: 'expose-loader',
-          options: 'jQuery'
-        },{
-          loader: 'expose-loader',
-          options: '$'
-        }]
-      }
-    ]
-  }
+    optimization: {
+        minimizer: [
+            new OptimizeCSSAssetsPlugin({}),
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                terserOptions: {
+                    ecma: 6,
+                },
+            }),
+        ],
+    },
+    entry: {
+        main: "./js/main.js",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "bundles"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: require.resolve("jquery"),
+                use: [
+                    {
+                        loader: "expose-loader",
+                        options: "jQuery",
+                    },
+                    {
+                        loader: "expose-loader",
+                        options: "$",
+                    },
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: "ts-loader",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
 };
